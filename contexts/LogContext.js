15043005263f32585a7041412d1adf2bb12b4aa8
bok <bok@bokui-Macmini.local>
2022-01-17 14:18:34 +0900

    DayLog ~ p.406

--- contexts/LogContext.js
+++ contexts/LogContext.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {createContext, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
+import logsStorage from '../storage/logsStorage';
 
 // const LogContext = createContext('안녕하세요.');
 
 const LogContext = createContext();
 
 export function LogContextProvider({children}) {
-  // 테스트 데이터 10개 생성
-  const [logs, setLogs] = useState(
-    Array.from({length: 10})
-      .map((_, index) => ({
-        id: uuidv4(),
-        title: `Log ${index}`,
-        body: `Log ${index}`,
-        date: new Date().toISOString(),
-      }))
-      .reverse(),
-  );
+  const initialLogsRef = useRef(null);
+  const [logs, setLogs] = useState([]);
 
-  // console.log('logs :', logs);
+  // // 테스트 데이터 10개 생성
+  // const [logs, setLogs] = useState(
+  //   Array.from({length: 10})
+  //     .map((_, index) => ({
+  //       id: uuidv4(),
+  //       title: `Log ${index}`,
+  //       body: `Log ${index}`,
+  //       date: new Date().toISOString(),
+  //     }))
+  //     .reverse(),
+  // );
+
   const onCreate = ({title, body, date}) => {
     const log = {
       id: uuidv4(),
@@ -40,6 +43,25 @@ export function LogContextProvider({children}) {
     const nextLogs = logs.filter(log => log.id !== id);
     setLogs(nextLogs);
   };
+
+  useEffect(() => {
+    // useEffect 내에서 async 함수를 만들고 바로 호출
+    // IIFE 패턴
+    (async () => {
+      const savedLogs = await logsStorage.get();
+      if (savedLogs) {
+        initialLogsRef.current = savedLogs;
+        setLogs(savedLogs);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (logs === initialLogsRef.current) {
+      return;
+    }
+    logsStorage.set(logs);
+  }, [logs]);
 
   return (
     <LogContext.Provider value={{logs, onCreate, onModify, onRemove}}>
